18127a22f79ae03e70d78e146c77023a07aee82a
erikbackman <erikbackman@users.noreply.github.com>
2022-07-17 02:02:21 +0200

    added some css

--- publish.el
+++ publish.el
@@ -43,9 +43,9 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
-;      org-html-head "<link rel=\"stylesheet\" href=\"./assets/style.css\" />"
-)
+
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
+                     <link rel=\"stylesheet\" href=\"../assets/style.css\" />")
 
 (setq org-html-validation-link nil)
 (org-publish-all t)
