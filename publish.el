191daf83b310e5060f2fd0b100e302e4f5c126f4
erikbackman <erikbackman@users.noreply.github.com>
2022-07-24 01:21:13 +0200

    cleanup

--- publish.el
+++ publish.el
@@ -3,50 +3,52 @@
 (setq package-user-dir (expand-file-name "./.packages"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
 (package-initialize)
-(package-refresh-contents)
 
-(unless
-    (package-installed-p 'htmlize)
- (package-install 'htmlize))
+(unless (package-installed-p 'htmlize)
+  (package-refresh-contents)
+  (package-install 'htmlize))
 
 (require 'ox-publish)
-(setq org-src-fontify-natively t)
 (require 'ox-html)
 (require 'htmlize)
-(setq htmlize-face-overrides nil)
 
-;; (setq htmlize-face-overrides
-;;       '(font-lock-warning-face \"black\"
-;; 			       font-lock-function-name-face \"red\"
-;; 			       font-lock-comment-face \"blue\"
-;; 			       default (:foreground \"dark-green\" :background \"yellow\")))
-
-;; There is a bug in ox-html, which doesn't check for empty string in
-;; properties, so `org-html-extension' should set using variable.
 (setq org-publish-project-alist
-      (list
-       (list "my-org-site"
-             :recursive t
-             :base-directory "./content"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./docs"
-             :with-author nil
-             :with-creator t
-             :with-toc nil  
-             :section-numbers nil
-             :time-stamp-file nil))) 
+      '(("notes"
+	 :base-directory "./content/notes"
+	 :base-extension "org"
+	 :publishing-function org-html-publish-to-html
+	 :publishing-directory "./docs/notes"
+	 :with-author nil
+	 :with-creator t
+	 :with-toc nil
+	 :section-numbers nil
+	 :time-stamp-file nil
+	 :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /> <link rel=\"stylesheet\" href=\"../assets/style.css\" /> "
+	 )
+	("assets"
+	 :base-directory "./assets"
+	 :base-extension "css"
+	 :publishing-directory "./docs/assets"
+	 :publishing-function org-publish-attachment)
+	("index"
+	 :base-directory "./content"
+	 :publishing-function org-html-publish-to-html
+	 :publishing-directory "./docs"
+	 :with-author nil
+	 :with-creator t
+	 :with-toc nil
+	 :section-numbers nil
+	 :time-stamp-file nil
+	 :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /> <link rel=\"stylesheet\" href=\"./assets/style.css\" /> ")
+	 ("website" :components ("index" "notes" "assets"))))
 
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-validation-link nil
+      org-src-fontify-natively t
+      htmlize-face-overrides nil)
 
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
-                     <link rel=\"stylesheet\" href=\"./assets/style.css\" />")
-
-
-(setq org-html-validation-link nil)
 (org-publish-all t)
 
