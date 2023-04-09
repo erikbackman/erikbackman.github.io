bda6b8db6959a447bd5282f9001efbd7a55f96ca
erikbackman <erikbackman@users.noreply.github.com>
2023-04-09 23:02:56 +0200

    cleanup

--- publish.el
+++ publish.el
@@ -18,46 +18,44 @@
 (require 'ox-html)
 (require 'htmlize)
 
-(let ((creator
-       (format
-	"Generated with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a> %s and <a href=\"https://orgmode.org\">Org</a> mode %s"
-	emacs-version
-	(org-version)))
-      (html-head
-       "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /> <link rel=\"stylesheet\" href=\"../assets/style.css\" /> "))
-  (setq org-publish-project-alist
-	`(("notes"
-	   :base-directory "./content/notes"
-	   :base-extension "org"
-	   :publishing-function org-html-publish-to-html
-	   :publishing-directory "./docs/notes"
-	   :with-author nil
-	   :with-creator t
-	   :creator ,creator
-	   :with-toc nil
-	   :section-numbers nil
-	   :time-stamp-file nil
-	   :html-preamble "<div id=bar><a href=../index.html>Index</a></div>"
-	   :html-head ,html-head
-	   )
-	  ("assets"
-	   :base-directory "./assets"
-	   :base-extension "css\\|ttf"
-	   :publishing-directory "./docs/assets"
-	   :publishing-function org-publish-attachment)
-	  ("index"
-	   :base-directory "./content"
-	   :publishing-function org-html-publish-to-html
-	   :publishing-directory "./docs"
-	   :with-author nil
-	   :with-creator t
-	   :creator ,creator
-	   :with-toc nil
-	   :section-numbers nil
-	   :time-stamp-file nil
-	   :html-head ,html-head)
-	  ("website"
-	   :components ("index" "notes" "assets")))))
+(setq creator
+      (format "Generated with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a> %s and <a href=\"https://orgmode.org\">Org</a> mode %s"
+	      emacs-version (org-version))
+
+      html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /> <link rel=\"stylesheet\" href=\"../assets/style.css\" /> "
+      
+      org-publish-project-alist
+      `(("notes"
+	 :base-directory "./content/notes"
+	 :base-extension "org"
+	 :publishing-function org-html-publish-to-html
+	 :publishing-directory "./docs/notes"
+	 :with-author nil
+	 :with-creator t
+	 :creator ,creator
+	 :with-toc nil
+	 :section-numbers nil
+	 :time-stamp-file nil
+	 :html-preamble "<div id=bar><a href=../index.html>Index</a></div>"
+	 :html-head ,html-head)
+	("assets"
+	 :base-directory "./assets"
+	 :base-extension "css\\|ttf"
+	 :publishing-directory "./docs/assets"
+	 :publishing-function org-publish-attachment)
+	("index"
+	 :base-directory "./content"
+	 :publishing-function org-html-publish-to-html
+	 :publishing-directory "./docs"
+	 :with-author nil
+	 :with-creator t
+	 :creator ,creator
+	 :with-toc nil
+	 :section-numbers nil
+	 :time-stamp-file nil
+	 :html-head ,html-head)
+	("website"
+	 :components ("index" "notes" "assets"))))
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
